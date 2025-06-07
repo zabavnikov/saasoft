59cd2bf762352ffd8c92bf622f77737ebaeace88
Denis <zabavnikov.com@gmail.com>
2025-06-07 08:14:25 +0700

    Поправил ошибки типизации.

--- src/components/form/AccountTypeSelect.vue
+++ src/components/form/AccountTypeSelect.vue
@@ -6,12 +6,14 @@ import { ACCOUNT_TYPE_LIST } from '../../constants.ts'
 const model = defineModel<Maybe<AccountType>>({
 	default: null,
 })
+
+const options = [...ACCOUNT_TYPE_LIST]
 </script>
 
 <template>
 	<Select
 		v-model="model"
-		:options="ACCOUNT_TYPE_LIST"
+		:options="options"
 		option-label="name"
 		option-value="id"
 		placeholder="Выберите тип записи"
